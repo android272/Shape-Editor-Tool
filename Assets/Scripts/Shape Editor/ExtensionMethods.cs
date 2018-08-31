7d00620477b2837bceaddd80ae0ed082ddf580e6
Andrew Pullins <android2772@gmail.com>
2018-08-31 09:11:11 -0400

    Added ToYZ() and ToXY() to ExtensionsMethods

--- Assets/Scripts/Shape Editor/ExtensionMethods.cs
+++ Assets/Scripts/Shape Editor/ExtensionMethods.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class ExtensionMethods {
-
     public static Vector2 ToXZ(this Vector3 v3)
     {
         return new Vector2(v3.x, v3.z);
     }
 
+    public static Vector2 ToYZ(this Vector3 v3) {
+        return new Vector2(v3.y, v3.z);
+    }
+
+    public static Vector2 ToXY(this Vector3 v3) {
+        return new Vector2(v3.x, v3.y);
+    }
 }
